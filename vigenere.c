c2fcda4935f8651cdaa4c420be15d5e163f89b6c
FANG <yongsan@vip.qq.com>
2017-01-09 21:15:15 +0800

    day 2
    明天继续战

--- vigenere.c
+++ vigenere.c
@@ -2,28 +2,59 @@
 # include <cs50.h>
 # include <ctype.h>
 # include <string.h>
+
+
 int main(int argc, char *argv[]){
-    // int i;
+    int len;
     char *key, *plaintext;
-    key = argv[1];
-    if (argc>2){    //这个if循环用来检查输入是否满足条件
+
+    if (argc>2||argv[1]==NULL){    //这个if循环用来检查输入个数
         printf("too much!\n");
         return 1;
     }
-
-    
-    else if (isalpha(key[0])){
-        for (int i=0, n=strlen(key);i<n;i++)
+    else { //检查输入是否满足条件
+        key = argv[1];
+        len = strlen(key);
+        for (int i=0;i<len;i++)
         { 
-            if (key[i]>='0'&&key[i]<='9') //使用ASCII码值进行检查非数字
-            {   printf("your input include number :%c\n", key[i]);
+            if (key[i]<'A'||key[i]>'z'||(key[i]>'Z'&&key[i]<'a')) //使用ASCII码值进行检查非数字
+            {   printf("invalid input:%c\n", key[i]);
                 return 1;
             }
         }
-            // return 1;
     }
     printf("plaintext:\n");
     plaintext = GetString();
-    
-    //加密部分.....
-}
+    printf("ciphertext:\n");
+    for(int i=0, j=0, n=strlen(plaintext);i<n;i++, j++)
+    {
+
+        if ((plaintext[i]>='A'&&plaintext[i]<='Z')||(plaintext[i]>='a'&&plaintext[i]<='z')) //留字符串部分
+        {   //检查明文是否为大小写字母
+            if (i%len==0&&i!=0) //key长度小于明文时调头
+                j = 0;
+            if (plaintext[i]>='A'&&plaintext[i]<='Z') //大写部分
+                {
+                plaintext[i] = plaintext[i]+(key[j]-'A'); //加密后
+                while (plaintext[i]>'Z')
+                    plaintext[i]-= 26;
+                printf("%c", plaintext[i]);
+                }
+                
+            if (plaintext[i]>='a'&&plaintext[i]<='z') //小写部分
+                {
+                    plaintext[i] = plaintext[i]+(key[j]-'a'); //加密后
+                    // printf("ok\n");
+                    //  if (plaintext[i]>'z')
+                    // printf("no%c", (plaintext[i]-('a'-'G')));
+                    printf("%c", plaintext[i]);
+                }
+            }else{ //输出非字符
+                printf("%c",  plaintext[i]);
+                j = j - 1;}
+        }
+    printf("\n");
+    return 0;
+    }
+
+
